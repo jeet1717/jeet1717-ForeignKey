2795303b24a42ebebc9f2751b8812102b6bc9bcf
Jeet Patel <pateljeet33@gmail.com>
2023-08-29 19:07:18 +0000

    completed

--- problem1.sql
+++ problem1.sql
@@ -1,1 +1,1 @@
-CREATE TABLE post (id SERIAL PRIMARY KEY, post VARCHAR(250), user_fk int REFERENCES site_user(id));
+CREATE TABLE post (id SERIAL PRIMARY KEY, post VARCHAR(250), user_fk int REFERENCES site_user(id))
